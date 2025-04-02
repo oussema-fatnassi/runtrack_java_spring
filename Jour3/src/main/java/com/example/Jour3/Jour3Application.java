fe7c3fd4c21a956202c4ff1198deda347e75ca0f
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2025-04-02 14:45:00 +0200

    Job2

--- Jour3/src/main/java/com/example/Jour3/Jour3Application.java
+++ Jour3/src/main/java/com/example/Jour3/Jour3Application.java
@@ -2,18 +2,11 @@ package com.example.Jour3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Jour3Application {
 
 	public static void main(String[] args) {
-		
-		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-
 		SpringApplication.run(Jour3Application.class, args);
 	}
 
--- Jour3/src/main/java/com/example/Jour3/repository/PersonRepository.java
+++ Jour3/src/main/java/com/example/Jour3/repository/PersonRepository.java
@@ -1,7 +1,7 @@
-package com.example.Jour3.repository;
+// package com.example.Jour3.repository;
 
-import com.example.Jour3.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.Jour3.model.Person;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
+// public interface PersonRepository extends JpaRepository<Person, Long> {
+// }
